e5e0bee24bddb3ad680caf280e15f696a1a879c7
Minchou99 <minhchaupt2903@gmail.com>
2022-09-15 15:46:03 +0700

    update

---
+++ sql/ss4_cac_ham_thong_dung_trong_sql/ss4_luyen_tap_cac_ham_thong_dung.sql
@@ -0,0 +1,13 @@
+use student_management_ss3;
+select * 
+from `subject`  where credit = (select max(credit) credit_lon_nhat from `subject`);
+
+select * from `subject` s
+inner join mark on s.sub_id=mark.sub_id and mark = (select max(mark) from mark); 
+
+select s.student_id,s.student_name, avg(m.mark) diem_trung_binh  from student s
+inner join mark m on s.student_id = m.student_id
+group by student_id
+order by avg(mark) desc; 
+
+
---
+++ sql/ss4_cac_ham_thong_dung_trong_sql/thuc_hanh_cac_ham_thong_dung.sql
@@ -0,0 +1,23 @@
+use student_management_ss3;
+select address, count(student_id) so_luong_hoc_vien
+from student
+group by address;
+
+select s.student_id, s.student_name, avg(mark)
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name;
+
+select s.student_id, s.student_name, avg(mark)
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name
+having avg(mark)>15;
+
+select s.student_id, s.student_name, avg(mark) 
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name;
+
+select s.student_id, s.student_name, avg(mark)
+from student s join mark m on s.student_id = m.student_id
+group by s.student_id, s.student_name
+having avg(mark) >= all (select avg(mark) from mark group by mark.student_id); 
+
