2b8dfbc73d44f2ae3d6d7155cd62858ee88e68fc
Minchou99 <minhchaupt2903@gmail.com>
2022-09-20 10:59:18 +0700

    update

--- sql/casestudy1/casestudy_1_20.sql
+++ sql/casestudy1/casestudy_1_20.sql
@@ -51,15 +51,15 @@ ORDER BY so_lan_dat_phong ;
        dv.ten_dich_vu,
        hd.ngay_lam_hop_dong,
        hd.ngay_ket_thuc,
-       sum(ifnull(dv.chi_phi_thue, 0))+sum(ifnull(hdct.so_luong, 0)*ifnull(dvdk.gia, 0)) tong_tien
+       (ifnull(dv.chi_phi_thue, 0))+sum(ifnull(hdct.so_luong, 0)*ifnull(dvdk.gia, 0)) tong_tien
 FROM khach_hang kh
 LEFT JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
 LEFT JOIN loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
 LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
 LEFT JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
 LEFT JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
-GROUP BY hd.ma_hop_dong;
- 
+GROUP BY hd.ma_hop_dong, kh.ma_khach_hang;
+
 -- task 6 Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 
 -- (Quý 1 là tháng 1, 2, 3).
@@ -134,7 +134,7 @@ WHERE year(hd.ngay_lam_hop_dong) = 2021
 GROUP BY thang
 ORDER BY thang;
 
--- task 10 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- task 10 Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
 -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, 
 -- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
 
