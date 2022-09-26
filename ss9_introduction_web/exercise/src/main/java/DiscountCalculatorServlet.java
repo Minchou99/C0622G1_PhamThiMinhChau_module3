040abac882d9aaf2b3ee44470dd4a9ac541f8c4d
Minchou99 <minhchaupt2903@gmail.com>
2022-09-26 16:53:53 +0700

    update

--- ss9_introduction_web/exercise/src/main/java/DiscountCalculatorServlet.java
+++ ss9_introduction_web/exercise/src/main/java/DiscountCalculatorServlet.java
@@ -5,23 +5,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 
 @WebServlet(name = "DiscountCalculatorServlet", value = "/convert")
 public class DiscountCalculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        double price = Double.parseDouble(request.getParameter("price"));
-        double discount = Double.parseDouble(request.getParameter("discount"));
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html;charset=UTF-8");
+            double price= Double.parseDouble(request.getParameter("price"));
+            BigDecimal listPrice = new BigDecimal(price);
+            double discountPercent = Double.parseDouble(request.getParameter("discount"));
 
-        double amount = price * discount * 0.01 ;
-        double payment = price - amount;
+            BigDecimal discountAmount = new BigDecimal(price * discountPercent * 0.01);
+            BigDecimal discountPrice = listPrice.subtract(discountAmount);
+            String description = request.getParameter("text");
 
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.println("<h1>List Price: " + price + "</h1>");
-        writer.println("<h1>Discount: " + discount + "</h1>");
-        writer.println("<h1>Discounted: " + amount + "</h1>");
-        writer.println("<h1>Payment: " + payment + "</h1>");
-        writer.println("</html>");
+            request.setAttribute("description", description);
+            request.setAttribute("listPrice", listPrice);
+            request.setAttribute("discountPercent", discountPercent);
+            request.setAttribute("discountAmount", discountAmount);
+            request.setAttribute("discountPrice", discountPrice);
+            request.getRequestDispatcher("/result.jsp").forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- ss9_introduction_web/exercise/src/main/webapp/index.jsp
+++ ss9_introduction_web/exercise/src/main/webapp/index.jsp
@@ -8,13 +8,13 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Information</title>
   </head>
   <body>
   <h2>Product Discount Calculator</h2>
   <form method="post" action="/convert">
-    <h5>Product Description</h5>
-    <p>That product is useful and comfortable. You should buy it!</p>
+    <label>Product Description: </label> <br/><br/>
+    <input type="text" name="text" /> <br/><br/>
     <label>List Price: </label><br/><br/>
     <input type="text" name="price" placeholder="Enter product's price: "/> <br/><br/>
     <label>Discount Percent: </label><br/><br/>
